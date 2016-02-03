7590e069ee79cdea6a1f11a2d32d4f93cddc9548
Jianfu Han <j52han@uwaterloo.ca>
2016-02-03 15:20:01 -0500

    first commit

---
+++ Roll-a-Ball/Assets/Script/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	public Text winText;
+
+	public Text countText;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag("picksup")) {
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count == 9) {
+			winText.text = "You Win";
+		}
+	}
+}
